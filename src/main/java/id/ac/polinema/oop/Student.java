039ccbd2e219ebb40f6adf3d37f6b213fc2e05ca
jiadaMizan <jiada1308@gmail.com>
2026-09-18 08:20:47 +0700

    1st try

--- src/main/java/id/ac/polinema/oop/Student.java
+++ src/main/java/id/ac/polinema/oop/Student.java
@@ -11,31 +11,28 @@ public class Student {
 
     // TODO: add fields: studentId (String), name (String), gpa (double)
 
-    /**
-     * Student constructor.
-     *
-     * @param studentId Student identification number
-     * @param name      Student's full name
-     * @param gpa       Grade Point Average (0.0 - 4.0)
-     */
+     
+     String studentId; 
+     String name;      
+     double gpa;       
+     
     public Student(String studentId, String name, double gpa) {
-        // TODO: store parameters into fields
-        throw new UnsupportedOperationException("Not implemented yet");
+        this.studentId = studentId;
+        this.name = name;
+        this.gpa = gpa;
+
     }
 
     public String getStudentId() {
-        // TODO: return studentId
-        throw new UnsupportedOperationException("Not implemented yet");
+        return studentId;
     }
 
     public String getName() {
-        // TODO: return name
-        throw new UnsupportedOperationException("Not implemented yet");
+        return name;
     }
 
     public double getGpa() {
-        // TODO: return gpa
-        throw new UnsupportedOperationException("Not implemented yet");
+        return gpa;
     }
 
     /**
@@ -47,7 +44,12 @@ public class Student {
      * @return graduation predicate
      */
     public String getPredicate() {
-        // TODO: implement the predicate logic per the rules above
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (gpa >= 3.5) {
+            return "Cum Laude";
+        } else if (gpa < 3.5 || gpa >= 3.0) {
+            return "Very Satisfactory";
+        } else {
+            return "Satisfactory";
+        }
     }
 }
